51ec4e8e12dfc416994095a8fb0470b9676ae590
SheetalSisodiya <sheetalsisodiya770@gmail.com>
2025-03-22 23:41:56 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import StudentModel
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'age', 'fees']
-
-admin.site.register(StudentModel, StudentAdmin)
